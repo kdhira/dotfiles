2a2fdb7d0fa3c8cf07e3226b84acae143ad4ac0a
Kevin Hira <kevin.hira@orionhealth.com>
2024-05-06 08:41:58 +1200

    Add aliases

--- zshrc.d/alias.sh
+++ zshrc.d/alias.sh
@@ -3,6 +3,13 @@
 # General aliases
 alias ll='ls -plaThG'
 
+# less ls
+lll() {
+    CLICOLOR_FORCE=1 ll $@ | less
+}
+
+alias b64='base64'
+
 # Encrypt/decrypt with id_rsa
 alias ssh-encrypt='openssl pkeyutl -encrypt -inkey ~/.ssh/id_rsa'
 alias ssh-decrypt='openssl pkeyutl -decrypt -inkey ~/.ssh/id_rsa'
@@ -25,4 +32,42 @@ dotfiles() {
         echo $_usage >&2
         return 1
     fi
+}
+
+rsamd5() {
+    openssl rsa -noout -modulus $@ | openssl md5
+}
+
+x509md5() {
+    openssl x509 -noout -modulus $@ | openssl md5
+}
+
+
+# MacOS pasteboard stuff
+alias pb='pbpaste'
+alias pbd='pbpaste | base64 -D'
+alias pbe='pbpaste | base64'
+alias pc='pbcopy'
+alias pcd='base64 -D | pbcopy'
+alias pce='base64 | pbcopy'
+
+# X509 utils
+alias x509='openssl x509'
+alias x509t='openssl x509 -text'
+
+# openssl s_client shortcuts
+sslping() {
+    if [[ $# -lt 1 ]]; then
+        echo "Usage: $0 <target>[port]"
+        return 1
+    fi
+    endpoint="$1"
+    if [[ "$endpoint" != *:* ]]; then
+        endpoint="$endpoint:443"
+    fi
+
+    : | openssl s_client -showcerts -connect $endpoint
+}
+sslx509() {
+    sslping $1 | openssl x509 -text
 }
