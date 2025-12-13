8a8b534a7827c1cf98f985a5bc80de5f16fa986e
Kevin Hira <kevin@hira.nz>
2025-12-14 10:03:02 +1300

    Updates to omp; PATH vars for go/rust

--- zshrc.d/env.sh
+++ zshrc.d/env.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+test -d "$HOME/go/bin" && export PATH="$HOME/go/bin:$PATH"
+
 # Always export ~/bin to PATH
 export PATH="$HOME/bin:$PATH"
+
+test -d "$HOME/.cargo/bin" && export PATH="$HOME/.cargo/bin:$PATH"
+
